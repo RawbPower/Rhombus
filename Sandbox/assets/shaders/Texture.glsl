a505e0af814babe73b7c8faa21a3b63d9a6b38da
RawbPower <RawbPower@users.noreply.github.com>
2023-11-08 01:08:32 +0000

    Fixes to Texture.glsl

--- Sandbox/assets/shaders/Texture.glsl
+++ Sandbox/assets/shaders/Texture.glsl
@@ -1,7 +1,7 @@
 // Basic Texture Shader
 
 #type vertex
-#version 330 core
+#version 450 core
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
@@ -13,7 +13,7 @@ uniform mat4 u_ViewProjection;
 
 out vec4 v_Color;
 out vec2 v_TexCoord;
-out float v_TextureIndex;
+out flat float v_TextureIndex;
 out float v_TilingFactor;
 
 void main() 
@@ -26,13 +26,13 @@ void main()
 }
 
 #type fragment
-#version 330 core
+#version 450 core
 
 layout(location = 0) out vec4 color;
 
 in vec4 v_Color;
 in vec2 v_TexCoord;
-in float v_TextureIndex;
+in flat float v_TextureIndex;
 in float v_TilingFactor;
 
 uniform sampler2D u_Textures[32];
@@ -52,6 +52,7 @@ void main()
 		case 1: texColor *= texture(u_Textures[1], v_TexCoord * v_TilingFactor); break;
 		case 2: texColor *= texture(u_Textures[2], v_TexCoord * v_TilingFactor); break;
 		case 3: texColor *= texture(u_Textures[3], v_TexCoord * v_TilingFactor); break;
+		case 4: texColor *= texture(u_Textures[4], v_TexCoord * v_TilingFactor); break;
 		case 5: texColor *= texture(u_Textures[5], v_TexCoord * v_TilingFactor); break;
 		case 6: texColor *= texture(u_Textures[6], v_TexCoord * v_TilingFactor); break;
 		case 7: texColor *= texture(u_Textures[7], v_TexCoord * v_TilingFactor); break;

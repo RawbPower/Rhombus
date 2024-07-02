1f6f9960a57d2caeec32b0d1345a2a22a1e57041
RawbPower <robjamespower@gmail.com>
2024-07-02 22:07:56 +0100

    Adding some new cards

---
+++ Rhombus-Editor/Patience/Assets/scripts/CardData_Monsters.lua
@@ -0,0 +1,26 @@
+CardData.Monsters =
+{
+	Goblin_Claws = 
+	{
+		Rank = 0,
+		Suit = "Wild",
+		Sprite = "textures/Cards/Card56.png",
+		PackingType = "Default"
+	},
+
+	Slime_Claws = 
+	{
+		Rank = 0,
+		Suit = "Wild",
+		Sprite = "textures/Cards/Card57.png",
+		PackingType = "Default"
+	},
+
+	Wolf_Claws = 
+	{
+		Rank = 0,
+		Suit = "Wild",
+		Sprite = "textures/Cards/Card58.png",
+		PackingType = "Default"
+	}
+}
